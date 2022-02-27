30c16398453ce9eba8f0d08432dc92e91ac48448
TheSingleOne <81616417+TheSingleOneYT@users.noreply.github.com>
2022-02-27 21:31:25 +0000

    Change some things

--- Batch/YT-DownloaderBatch.bat
+++ Batch/YT-DownloaderBatch.bat
@@ -1,5 +1,5 @@
 :: Batch By TheSingleOneYT, youtube-dl from https://github.com/ytdl-org/
-:: More/newer versions of this batch may be available at https://github.com/TheSingleOneYT/YT-Downloader-Batch
+:: More/newer versions of this batch may be available at https://github.com/TheSingleOneYT/YT-Downloader-Scripts/tree/main/Batch
 
 @ECHO OFF
 choice /c 1234 /n /m "1 help, 2 Download Video & Audio, 3 Download ONLY AUDIO, 4 Update youtube-dl.exe: "
@@ -64,10 +64,9 @@ goto :eof
 :help
 echo How to use this Batch File! (BASIC)
 echo 1) Make sure to have youtube-dl.exe (https://yt-dl.org/latest/youtube-dl.exe) installed in THE SAME DIRECTORY as this .bat
-echo 2) Install Python (https://www.python.org/downloads/windows/)
-echo 3) Install Microsoft Visual C++ 2010 x86 Redistributable (https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe)
-echo 4) Run this Batch File again and type 2. Then, on the next prompt, paste the YT video URL.
-echo 5) It should now begin downloading the video, wait and enjoy!
+echo 2) Install Microsoft Visual C++ 2010 x86 Redistributable (https://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/vcredist_x86.exe)
+echo 3) Run this Batch File again and type 2. Then, on the next prompt, paste the YT video URL.
+echo 4) It should now begin downloading the video, wait and enjoy!
 
 pause
 goto :eof
